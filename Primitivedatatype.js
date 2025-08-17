5c6fe34878ab1175f141c7cd874bef34f90bfeb9
abhuvana1311 <abhuvana1311@gmail.com>
2025-08-16 23:10:01 -0400

    Add files via upload

---
+++ Primitivedatatype.js
@@ -0,0 +1,25 @@
+var city = "FLorida"
+console.log("I Live in" + city);
+console.log("Value :", city);
+console.log("Type :", typeof city);
+
+let marks = 85;
+console.log(marks+10);
+console.log("Value :", marks);
+console.log("Type :", typeof marks);
+
+const isWeekend = true;
+
+if(isWeekend) {
+    console.log("Relax");
+    }
+    else
+    {
+        console.log("Work");
+    }
+    console.log("Value :", isWeekend);
+    console.log("Type :", typeof isWeekend);
+
+    var futureGoal;
+    console.log("Future Goal :", futureGoal);
+    console.log("Type :", typeof futureGoal);

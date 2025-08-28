c7f4cd8a27213a6b91bd2ba76084f68d249997cb
abhuvana1311 <abhuvana1311@gmail.com>
2025-08-28 13:40:06 -0400

    Add files via upload

---
+++ FrequentCharacter.js
@@ -0,0 +1,10 @@
+let GName = "Bhuvana"
+let count = 0
+for ( let i = 0; i<=GName.length; i++)
+{ 
+  if(GName.charAt[i]==="a")
+{
+  count = count+1;
+}
+}
+console.log("Number of Character of the Letter a " + count)

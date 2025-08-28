37cd5cd1ba1d93eea4cc387090b2a14a1c04f51a
abhuvana1311 <abhuvana1311@gmail.com>
2025-08-28 11:32:24 -0400

    Add files via upload

---
+++ Asychronous.js
@@ -0,0 +1,10 @@
+function browserPage(){
+    setTimeout(()=> {
+        console.log("Loading the image ") }, 2000);
+        console.log("Working with Asychronous Call")}
+function elementText()
+{
+    console.log("Loading the Element Text ")
+}
+browserPage()
+elementText()

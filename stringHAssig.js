13e4ec9e2f990c71abe6240f233a545ecfb72d01
abhuvana1311 <abhuvana1311@gmail.com>
2025-08-29 12:14:10 -0400

    Add files via upload

---
+++ stringHAssig.js
@@ -0,0 +1,9 @@
+// Example 1 :- Return the length of the last word in the string
+let s = "Hello World"
+let lastword = (s.substring(6,11)) // Output is World
+let count = 0
+for ( let i = 0; i<lastword.length; i++)
+{
+  count++ // count = count + 1
+}
+console.log(" Print the Length for the last world in the given string  : " + count)

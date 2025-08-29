21406c5735ca1a46c837ec0309bd770fd9453ca7
abhuvana1311 <abhuvana1311@gmail.com>
2025-08-29 13:48:04 -0400

    Add files via upload

---
+++ stringHAs1.js
@@ -0,0 +1,10 @@
+// Example 2 :- Return the length of the last word in the string
+let word = "  fly me  to  the moon"
+let lastword = (word.slice(-4)) // Output is moon
+console.log (" Print : " + lastword)
+let count1 = 0
+for ( i = 0; i <lastword.length; i++)
+{
+count1++ // count1 = count1 + 1
+}
+console.log (" Print the Length for the last word in the given string : " + count1)
